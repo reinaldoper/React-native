757797dcdaedd7a3fcc0b1838fc54769985ec37a
reinaldoper <reinaldoper83@gmail.com>
2025-03-23 19:39:54 -0400

    fix: Ajusta função fetch

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,30 +1,59 @@
-import React, { useEffect, useState } from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { View, ScrollView } from 'react-native';
-import { Text, Button, Card } from 'react-native-elements';
-import styles from '../styles';
+import React, { useEffect, useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { View, ScrollView, ActivityIndicator } from "react-native";
+import { Text, Button, Card } from "react-native-elements";
+import styles from "../styles";
+import fetchPosts from "../service/Fetch.js";
 
 export default function HomeScreen({ navigation }) {
   const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const fetchPostsUser = async () => {
+    try {
+      const data = await fetchPosts();
+      setPosts(data);
+    } catch (error) {
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-      .then(response => response.json())
-      .then(data => setPosts(data))
-      .catch(error => console.error(error));
+    fetchPostsUser();
   }, []);
+
+  if (loading) {
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator size="large" color="#0000ff" />
+      </View>
+    );
+  }
+
+  if (error) {
+    return (
+      <View style={styles.container}>
+        <Text>Erro ao carregar os posts: {error.message}</Text>
+      </View>
+    );
+  }
 
   return (
     <View style={styles.container}>
       <ScrollView>
-        {posts.map(post => (
+        {posts.map((post) => (
           <Card key={post.id}>
             <Card.Title>{post.title}</Card.Title>
             <Card.Divider />
             <Text style={styles.body}>{post.body}</Text>
             <Button
               title="Ver Detalhes"
-              onPress={() => navigation.navigate('Details', { postId: post.id })}
+              onPress={() =>
+                navigation.navigate("Details", { postId: post.id })
+              }
             />
           </Card>
         ))}
---
+++ service/Fetch.js
@@ -0,0 +1,10 @@
+const fetchPosts = async () => {
+
+    const response = await fetch(`https://jsonplaceholder.typicode.com/posts/`);
+    const data = await response.json();
+
+    return data;
+}
+
+
+export default fetchPosts;
